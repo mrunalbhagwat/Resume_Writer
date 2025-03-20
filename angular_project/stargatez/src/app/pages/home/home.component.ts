6b8452f480a5c23e0cf9cd8c07b667accf62df97
Mrunal Bhagwat <mrunal.bhagwat12@gmail.com>
2025-03-20 15:57:01 +0530

    ui chnahes

--- angular_project/stargatez/src/app/pages/home/home.component.ts
+++ angular_project/stargatez/src/app/pages/home/home.component.ts
@@ -4,31 +4,55 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
   
+  typingSpeed: number = 100; // Speed in milliseconds
+  typingTimeout: any; // Store timeout reference
+
+  constructor(private route: ActivatedRoute) { }
+
   videoLink: any = 'assets/videos/hom_vid.mp4';
 
   cards: any = [
     {
       name: 'Contract Placement',
-      imagePath: '/assets/icons/contract_placement.png'
-    }, {
+      imagePath: '/assets/icons/contract_placement.png',
+      fullText: "We provide skilled contract professionals.",
+      displayedText: "",
+      typingTimeout: null
+    },
+    {
       name: 'Contract Hire',
-      imagePath: '/assets/icons/contract_hire.png'
-    }, {
+      imagePath: '/assets/icons/contract_hire.png',
+      fullText: "Hire talent on a contract-to-hire basis.",
+      displayedText: "",
+      typingTimeout: null
+    },
+    {
       name: 'Permanent Placements',
-      imagePath: '/assets/icons/permanent_placements.png'
-    }, {
+      imagePath: '/assets/icons/permanent_placements.png',
+      fullText: "Find the best full-time employees.",
+      displayedText: "",
+      typingTimeout: null
+    },
+    {
       name: 'Managed Project Services',
-      imagePath: '/assets/icons/managed_project.png'
-    }, {
+      imagePath: '/assets/icons/managed_project.png',
+      fullText: "Let us handle your projects end-to-end.",
+      displayedText: "",
+      typingTimeout: null
+    },
+    {
       name: 'Global Recruitment',
-      imagePath: '/assets/icons/global_icon.png'
+      imagePath: '/assets/icons/global_icon.png',
+      fullText: "Recruit talent from around the world.",
+      displayedText: "",
+      typingTimeout: null
     }
-  ]
+  ];
+
   ngOnInit() {
     this.route.fragment.subscribe(fragment => {
       if (fragment) {
@@ -37,4 +61,18 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  typeWriterEffect(card: any, index: number) {
+    if (index < card.fullText.length) {
+      card.typingTimeout = setTimeout(() => {
+        card.displayedText += card.fullText.charAt(index);
+        this.typeWriterEffect(card, index + 1);
+      }, this.typingSpeed);
+    }
+  }
+
+  restartTyping(card: any) {
+    clearTimeout(card.typingTimeout); // Stop any ongoing typing
+    card.displayedText = ""; // Reset text
+    this.typeWriterEffect(card, 0); // Restart typing effect
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,22 @@ module.exports = {
       colors: {
         primary: '#14176C',
         primary_dark: '#0042C8'
-      }
+      },
+      keyframes: {
+        typing: {
+          '0%': { width: '0%' },
+          '100%': { width: '100%' }
+        },
+        blink: {
+          '50%': { opacity: '0' }
+        }
+      },
     },
+    animation: {
+      typing: 'typing 3s steps(40, end) forwards', // Added "forwards" to keep text
+      blink: 'blink 1s infinite'
+    }
   },
-  plugins: [],
-}
+    plugins: [],
+  };
 
