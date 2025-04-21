e317ca4d1957e6893d75cbc5bff5a3c7225c2d00
Aashish Bhagwat <aashishbhagwat@Aashishs-MacBook-Pro.local>
2025-04-21 16:01:19 +0530

    fix some page

--- angular_project/stargatez/src/app/pages/resume-writer/resume-writer.component.ts
+++ angular_project/stargatez/src/app/pages/resume-writer/resume-writer.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var AOS: any;
 
 @Component({
   selector: 'app-resume-writer',
   templateUrl: './resume-writer.component.html',
   styleUrl: './resume-writer.component.scss'
 })
-export class ResumeWriterComponent {
+export class ResumeWriterComponent implements OnInit {
   videoLink: any = 'assets/videos/resume_writer.mp4';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      if (typeof AOS !== 'undefined') {
+        AOS.init({
+          duration: 1000,
+          once: true
+        });
+      }
+    }, 0);
+  }
 
 }
