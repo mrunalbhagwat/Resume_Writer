33538d78576e65526f8b51055f1f1227332a2ef8
Aashish Bhagwat <aashishbhagwat@Aashishs-MacBook-Pro.local>
2025-06-12 12:56:24 +0530

    Links updated

--- angular_project/stargatez/src/assets/constants/app.constants.ts
+++ angular_project/stargatez/src/assets/constants/app.constants.ts
@@ -2,7 +2,7 @@ export const appConstants = {
     contactAPIs: 'https://creativehand.co.in/api/portfolio/',
     baseURLAdminAPIs: 'https://portfolio-api.creativehand.co.in',
     GEMINI_API_KEY: 'AIzaSyDw0cLcBMozFsjYnQpc6790hDyA9_2fMDk',
-    careerFluteAppURL: 'https://app.careerflute.com'
+    careerFluteAppURL: 'https://careerflute.com'
 };
   
   
